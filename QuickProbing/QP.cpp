a50ea4536038e063f1c6428c855a2820377b5d98
GuoYunZheSE <guoyunzhe.SE@gmail.com>
2017-07-14 16:01:01 +0800

    use vector

--- QuickProbing/QP.cpp
+++ QuickProbing/QP.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 int main()
 {
-	double** Matrix;
-	Matrix = new double*[];
+	int R, T;
+	vector<vector<double>> Matrix(R, vector<double>(T));//The Dataset Matrix
 	double alpha;//The threshold of Matrix.
 	int min_r;//The minmum number of Rows of the minning pattern.
 	int min_t;//The minmum number of Column of the minning pattern.
 
-	double*** upsilon;//The set of submatrices of Matrix.
-	upsilon = new double**[];
+	vector<vector<vector<double>>> upsilon;//The set of submatrix
 
-	int *CR = new int[];//The set of Rows that has been mined for at least one times.
-	int *CT = new int[];//The set of Columns that has been mined for at least one times.
-	
+	vector<int> CR(0);//The set of Rows that has been mined for at least one times.
+	vector<int> CT(0);//The set of Columns that has been mined for at least one times.
+	vector<int> tau(0);//The set of Columns available for expansion.
+
+	bool CR_Change = true;
+	bool CT_Change = true;
+	while (CR_Change&&CT_Change)//IF CR and CT don't converge
+	{
+		vector<int> Q(0);//The set of a SOPSM's Columns.
+		T = Matrix[0].size;
+		for (int i = 0; i < T; i++)//tau=T.
+		{
+			tau[i] = i;
+		}
+		 
+	}
 }
